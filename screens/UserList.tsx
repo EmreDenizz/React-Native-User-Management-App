89663165e0ee76021087657cc9ebf718eb978960
Emre Deniz <emre.deniz@outlook.com>
2024-04-10 00:44:07 -0400

    feat(list users): api integration for fetching users

--- screens/UserList.tsx
+++ screens/UserList.tsx
@@ -1,41 +1,75 @@
-import React from 'react';
+import React, { useState, useEffect } from 'react';
 import { TouchableOpacity, ScrollView, SafeAreaView, Text, Alert, View } from 'react-native';
 import { Card } from 'react-native-paper';
 import { Ionicons } from '@expo/vector-icons';
-import { useNavigation } from '@react-navigation/native';
+import { useNavigation, useFocusEffect } from '@react-navigation/native';
 import { useSelector } from 'react-redux';
 
 import styles from '../lib/Styles';
 import { User } from '../lib/Types';
 
 export default function UserListScreen() {
+  const [userList, setUserList] = useState<User[] | []>([]);
+  const [isMsgVisible, setIsMsgVisible] = useState(false)
+
   const navigation = useNavigation();
+
   const newUser = useSelector((state: any) => state.newUser);
 
-  const userList: User[] = [
-    { id: 1, name: 'Johndfsf Doedfgdfg', email: 'john@test.com', role: 'Admin', phone: '4376665544', age: 21  },
-    { id: 2, name: 'Jane Green', email: 'jane@test.com', role: 'User', phone: '4376665545', age: 22 },
-    { id: 3, name: 'Sam Green', email: 'sam@test.com', role: 'Viewer', phone: '4376665546', age: 23 },
-  ];
+  // API URL
+  const apiUrl = "http://127.0.0.1:3000";
 
-  const deleteUser = (userId: number) => {
-    console.log('Deleted user with ID:', userId);
+  // Get all patients from API
+  const getUsersFromAPI = async() => {
+    await fetch(apiUrl+"/users").
+      then((response) => response.json()).
+      then((json) => {
+        setUserList(json.reverse());
+      })
+      .catch((error) => {
+        console.error(error);
+      })
+  }
+
+  const deleteUser = (id: number) => {
+    fetch(apiUrl+'/users/'+id, {method:"DELETE"})
+      .then(data => {
+        setIsMsgVisible(true);
+        getUsersFromAPI();
+      })
+      .catch((error) => {
+        console.error(error);
+      })
   };
 
-  const handleDelete = (userId: number, userName: string) => {
+  const handleDelete = (id: number, userName: string) => {
     Alert.alert(
       'Confirm Deletion',
       `Are you sure to delete ${userName}?`,
       [
         { text: 'Cancel', style: 'cancel' },
-        { text: 'Delete', onPress: () => deleteUser(userId) },
+        { text: 'Delete', onPress: () => deleteUser(id) },
       ]
     );
   };
 
+  // Fetch users from API when focus
+  useFocusEffect(
+    React.useCallback(() => {
+      getUsersFromAPI();
+    }, [])
+  );
+
+  useEffect(() => {
+    setTimeout(() => {
+      setIsMsgVisible(false);
+    }, 2000);
+  }, [isMsgVisible])
+
   return (
     <SafeAreaView>
       <ScrollView contentContainerStyle={styles.container}>
+        {isMsgVisible && <Text style={styles.successMsg}>User deleted succesfully.</Text>}
         {userList.map((user) => (
           // @ts-ignore
           <TouchableOpacity key={user.id} onPress={() => {navigation.navigate('Details', { user });}}>
